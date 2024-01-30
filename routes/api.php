17d7bad0310730f730899c6265d2446ab63d9939
Tony Montemuro <tonyamontemuro@gmail.com>
2024-01-29 20:56:48 -0600

    added blog api route to api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +23,7 @@ Route::middleware('auth:sanctum')->group(function () {
     });
     Route::post("/logout", [AuthController::class, "logout"]);
     Route::apiResource("/users", UserController::class);
+    Route::apiResource("/blog", BlogController::class);
 });
 
 Route::post("/signup", [AuthController::class, "signup"]);

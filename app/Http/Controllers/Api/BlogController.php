a17fec036adf30aafceddace1b56f9136dddd2b4
Tony Montemuro <tonyamontemuro@gmail.com>
2024-01-29 20:53:22 -0600

    set up blog controller for api

--- app/Http/Controllers/Api/BlogController.php
+++ app/Http/Controllers/Api/BlogController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Blog;
 use Illuminate\Http\Request;
+use App\Http\Resources\BlogResource;
+use App\Http\Requests\StoreBlogRequest;
 
 class BlogController extends Controller
 {
@@ -13,15 +15,19 @@ class BlogController extends Controller
      */
     public function index()
     {
-        //
+        return BlogResource::collection(
+            Blog::query()->orderBy("created_at", "desc")->paginate(10)
+        );
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreBlogRequest $request)
     {
-        //
+        $data = $request->validated();
+        $blog = Blog::create($data);
+        return response(new BlogResource($blog), 201);
     }
 
     /**
@@ -29,15 +35,7 @@ class BlogController extends Controller
      */
     public function show(Blog $blog)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Blog $blog)
-    {
-        //
+        return new BlogResource($blog);
     }
 
     /**
@@ -45,6 +43,10 @@ class BlogController extends Controller
      */
     public function destroy(Blog $blog)
     {
-        //
+        if ($this->user()->id !== $blog->user_id) {
+            return response(['error' => 'You are not authorized to delete this blog.'], 403);
+        }
+        $blog->delete();
+        return response("", 204);
     }
 }
